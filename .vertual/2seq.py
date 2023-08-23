7450325218ba0af6714a8718014b336e3ae11017
KonstantinKolesov <P-A-T-R-I-O-T-kolesow@yandex.ru>
2023-08-23 18:19:28 +0300

    Финал

--- .vertual/2seq.py
+++
@@ -1,9 +0,0 @@
-user_list = []
-number_user_list = input("Введите в списк значения через ',' ';' '/' : ").replace(';', ',').replace('/', ',').split(',')
-sorted_directions = sorted(number_user_list)
-ready_list = [int(i) for i in sorted_directions]
-for i in ready_list:
-    if ready_list.count(i) == 1:
-        user_list.append(i)
-
-print(str(user_list).strip('[]'))
--- .vertual/3seq.py
+++
@@ -1,4 +0,0 @@
-user_list_1 = input("Введите в списк значения через ',' ';' '/' : ").replace(';', ',').replace('/', ',').split(',')
-user_list_2 = input("Введите в списк значения через ',' ';' '/' : ").replace(';', ',').replace('/', ',').split(',')
-remove_element = [int(i) for i in user_list_1 if i not in user_list_2]
-print(str(remove_element).strip('[]'))
